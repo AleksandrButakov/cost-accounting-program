da7140c0acc71a8d85a5dfeba09619e9f3fe9139
Aleksandr Butakov <anbngm@gmail.com>
2022-11-21 18:47:22 +0400

    Added DataSourceProvider

--- src/main/java/com/anbngm/db/DataSourceProvider.java
+++ src/main/java/com/anbngm/db/DataSourceProvider.java
@@ -1,4 +1,25 @@
 package com.anbngm.db;
 
-public class DataSourceProvider {
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+
+public enum DataSourceProvider {
+    INSTANCE;
+
+    MysqlDataSource ds;
+
+    public DataSource getDataSource() {
+        if (ds == null) {
+            ds = new MysqlDataSource();
+            ds.setServerName("localhost");
+            ds.setPortNumber(3306);
+            ds.setDatabaseName("some-mysql");
+            ds.setUser("root");
+            ds.setPassword("pass");
+
+        }
+        return ds;
+    }
+
 }
